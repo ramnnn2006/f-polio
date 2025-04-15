674f9192422f62fafcb17f126c964347d36ae4ed
P.H. RAMAKRISHNAN <phrkvvp@gmail.com>
2025-04-15 23:04:00 +0530

    initial

---
+++ config.js
@@ -0,0 +1,71 @@
+window.PorfolioConfig = {
+  name: 'Ramakrishnan P H   ',
+  title: 'Student | Developer ',
+  descr: 'a comp sci student from vit , passionate abt development & focused on improving  technical and interpersonal skills.',
+  skills: [
+    {
+      name: 'operating systems',
+      description: 'android . windows . linux'
+    },
+    {
+      name: 'Languages',
+      description: ' python .  c . cpp . html . css .  js  . flask ( planning on exploring more into backend development later down the road ) '
+    },
+    {
+      name: 'utilities',
+      description: 'github . gcp . arduino'
+    }
+    
+  ],
+  works: [
+    {
+      name: 'AURA',
+      link: 'https://github.com/ramnnn2006/AURA',
+      description: 'AURA , [ Automated User Response Assistant  ] that integrates voice commands, screenshot and photo capture, to-do list management, and platform searches (YouTube, Google, Spotify). It streamlines user interactions through voice recognition and automation.'
+    },
+    {
+      name: 'StarNet-QR',
+      link: 'https://starnet-epi6.onrender.com/',
+      description: 'StarNet-QR is a web-based WiFi QR Code Generator that allows users to quickly generate a QR code for their WiFi network by entering the SSID and password. Scanning the generated QR code allows instant connection without manually entering credentials'
+    },
+    {
+      name: 'forbidden area.',
+      link: './random.txt',
+      description: ' some of my projects which i started are half done and forgotten for months.i might put em up here if i ever complete it '
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Football',
+      description: 'Love playing and watching the game'
+    },
+    {
+      name: 'Photography',
+      description: 'althou my phone camera is bad , i try to click photos'
+    },
+    {
+      name: 'idling',
+      description: 'just sitting idle.'
+    },
+    {name: '???', description: ' not found yet'}
+  ],
+  contacts: {
+    email: 'phrkvvp@gmail.com',
+    mobileNo: '+91 7306124700',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/ramnnn2006'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://linkedin.com/in/ramnnn'
+      },
+      {
+        name: 'Instagram',
+        link: 'https://instagram.com/raamm__007'
+      }
+      
+    ]
+  }
+};
